59887f676a4a9aa9c2703aa307be0bd29885b5f2
FoundDream <ziwensong.cs@gmail.com>
2026-01-31 03:14:45 +0800

    Initial Commit

---
+++ Gleam/GleamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GleamApp.swift
+//  Gleam
+//
+//  Created by Song Ziwen on 2026/1/31.
+//
+
+import SwiftUI
+
+@main
+struct GleamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
